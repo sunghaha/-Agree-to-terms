3e5b9f266c50908a13a7a552e9460b8ac252c9dd
sunghaha <sungha4779@naver.com>
2022-08-17 17:51:54 +0900

    first commit

---
+++ script/script.js
@@ -0,0 +1,42 @@
+// let input = 'input:checkbox[name="chkbox"]';
+let TOSckb = $("#TOS_ckb").is(":checked");
+let Privacyckb = $("#Privacy_ckb").is(":checked");
+let shopInfckb = $("shopInf_ckb").is(":checked");
+
+//////////// 개별체크박스 전체 선택 //////////
+let inputTotal = document.getElementsByName("chkbox");
+
+$("input#total_ckb").click(function(){
+if ($("#total_ckb").is(":checked")) {
+    $(inputTotal).prop('checked',true);
+}else{
+    $(inputTotal).prop('checked',false);
+}
+});
+
+//////////// 하나의 체크박스 해제시 전체동의 체크박스 비활성화 //////////
+$(inputTotal).click( function(){
+    let checked = $(this).is(":checked");
+    if (!checked) {
+        $("#total_ckb").prop('checked', false);
+    }
+});
+//////////// 개별 체크박스 모두 체크시 전체동의 체크박스 활성화 //////////
+// $(function(){
+    // let checked= $(inputTotal).each(function(){
+    //     $(this).is(":checked"); 
+    // });
+    if (TOSckb&&Privacyckb&&shopInfckb) {
+        $("#total_ckb").prop('checked', true);
+    }
+// });
+//////////// 필수동의 체크시 //////////
+
+$("#join").click(function(){
+    
+    if ( TOSckb && Privacyckb ) {
+        $(this).prependTo(form);
+    } else {
+        alert("필수 약관 동의필요");
+    }
+});
